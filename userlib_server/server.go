80ab4371338dcbd7b550da75142cb654694560d7
Opposolis <wmsjshihao@gmail.com>
2024-04-04 16:47:20 +0800

    change the log output to a local file

--- userlib_server/server.go
+++ userlib_server/server.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	// 创建一个文件，用于存储日志
+	logFile, err := os.OpenFile("server.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal("Failed to open log file:", err)
+	}
+	defer logFile.Close()
+
+	// 设置日志输出到文件
+	log.SetOutput(logFile)
+
 	// 注册处理函数
 	http.HandleFunc("/datastoreGet", postHandler_datastoreGet)
 	http.HandleFunc("/keystoreSet", postHandler_keystoreSet)
